909df86dfda561b036df59075ec7145b1293b2c6
Krishna Patel <krishna.ricky.patel@gmail.com>
2021-01-13 15:04:06 -0500

    💄 Fix heatmap positioning - add labels to heatmap - add extra years

--- app/components/calendar-heatmap.js
+++ app/components/calendar-heatmap.js
@@ -9,19 +9,26 @@ export default class CalendarHeatmapComponent extends Component {
     const maxWeeks = 53; // to get either tail ends
     const margin = 1;
     const squareSize = 12;
-    const width = Math.ceil(maxWeeks * squareSize);
-    const height = Math.ceil(7 * squareSize + margin);
+
+    const years = d3.group(this.args.data, (d) =>
+      new Date(d.dates).getUTCFullYear()
+    );
+    const width = Math.ceil(maxWeeks * squareSize + 48); //TODO: get rid of the manual adjustment
+    const yearHeight = Math.ceil(7 * squareSize + margin);
+    const containerHeight = Math.ceil(yearHeight * years.size + 40);
 
     const timeWeek = d3.utcSunday;
     const countDay = (d) => d.getUTCDay();
-
-    //TODO: Add labels for weekday and year
+    const formatDay = (d) => {
+      console.log(d);
+      return ["", "Mon", "", "We", "", "Fri", ""][d];
+    };
 
     const svg = d3
       .select(element)
       .append("svg")
       .attr("width", width + margin * 2)
-      .attr("height", height + margin * 2)
+      .attr("height", containerHeight + margin * 2)
       .append("g")
       .attr("transform", `translate(${margin}, ${margin})`);
     const heatColor = d3
@@ -29,10 +36,43 @@ export default class CalendarHeatmapComponent extends Component {
       .range(["white", "red"])
       .domain([2000, 3000]);
 
-    svg
+    const group = svg.append("g");
+
+    const year = group
+      .selectAll("g")
+      .data(years)
+      .join("g")
+      .attr(
+        "transform",
+        (d, i) => `translate(50, ${yearHeight * i + i * squareSize * 1.5})`
+      );
+
+    year
+      .append("text")
+      .attr("x", -Math.ceil(yearHeight / 4))
+      .attr("y", -35)
+      .attr("text-anchor", "end")
+      .attr("font-size", 16)
+      .attr("font-weight", 550)
+      .attr("transform", "rotate(270)")
+      .text((d) => d[0]);
+
+    year
+      .append("g")
+      .attr("text-anchor", "end")
+      .selectAll("text")
+      .data(d3.range(7))
+      .join("text")
+      .attr("x", -5)
+      .attr("y", (d) => (d + 0.5) * squareSize)
+      .attr("dy", "0.31em")
+      .attr("font-size", 12)
+      .text(formatDay);
+
+    year
       .append("g")
       .selectAll("rect")
-      .data(this.args.data, (d) => d.calories_passive + d.calories_active)
+      .data((d) => d[1])
       .join("rect")
       .attr("width", squareSize - 2)
       .attr("height", squareSize - 2)
@@ -40,15 +80,16 @@ export default class CalendarHeatmapComponent extends Component {
         "x",
         (d) =>
           timeWeek.count(d3.utcYear(new Date(d.dates)), new Date(d.dates)) *
-            squareSize +
-          10
+          squareSize
       )
       .attr("y", (d) => countDay(new Date(d.dates)) * squareSize + 0.5)
       .attr("fill", (d) => heatColor(d.calories_active + d.calories_passive))
       .append("title")
       .text(
         (d) =>
-          `${d.dates}: ${(d.calories_active + d.calories_passive).toFixed(2)}`
+          `${d.dates}: ${(d.calories_active + d.calories_passive).toFixed(
+            2
+          )} calories`
       );
   }
 }
